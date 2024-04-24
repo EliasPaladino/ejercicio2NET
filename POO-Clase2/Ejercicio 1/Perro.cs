8f0f5f0cd789fa443651f08de4e699a988cbe1db
epaladino <devopstesting@cdainfo.com>
2024-04-23 23:49:27 -0300

    Ejercicio 2

---
+++ POO-Clase2/Ejercicio 1/Perro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Clase2.Ejercicio_1
+{
+    public class Perro : Animal
+    {
+        public override void EmitirSonido()
+        {
+            Console.WriteLine("Woof woof");
+        }
+    }
+}
---
+++ POO-Clase2/Ejercicio 3/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Clase2.Ejercicio_3
+{
+    public class Circulo : FiguraGeometrica
+    {
+
+        public double Radio {  get; set; }
+        public override double calcularArea()
+        {
+            return Math.PI * Math.Pow(this.Radio, 2);
+        }
+
+        public override double calcularPerimetro()
+        {
+            return Math.PI * 2 * this.Radio;
+        }
+    }
+}
